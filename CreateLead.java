1107fa710a3d476a83439eefe2c82365dbd73a36
karthikanav <134178125+karthikanav@users.noreply.github.com>
2023-05-29 21:53:42 -0500

    Add files via upload
    Week2.Day1 Create and Edit Lead assignment

---
+++ CreateLead.java
@@ -0,0 +1,37 @@
+package seleniumlaunch1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class CreateLead {
+
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver Driver=new ChromeDriver();
+		Driver.manage().window().maximize();
+		Driver.get("http://leaftaps.com/opentaps/control/login");
+		Driver.findElement(By.id("username")).sendKeys("demosalesManager");
+		Driver.findElement(By.id("password")).sendKeys("crmsfa");
+		Driver.findElement(By.className("decorativeSubmit")).click();
+		Driver.findElement(By.linkText("CRM/SFA")).click();
+		Driver.findElement(By.partialLinkText("Create Lead")).click();
+		Driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+		Driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Karthika");
+		Driver.findElement(By.id("createLeadForm_lastName")).sendKeys("nav");
+		Driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("K");
+		Driver.findElement(By.name("departmentName")).sendKeys("Chemical");
+		Driver.findElement(By.id("createLeadForm_description")).sendKeys("Need more practice");
+		Driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("21Karthika@gmail.com");
+		WebElement state = Driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select select=new Select(state);
+		select.selectByValue("NY");
+		Driver.findElement(By.className("smallSubmit")).click();
+		Thread.sleep(2000);
+		Driver.close();
+		
+			// TODO Auto-generated method stub
+
+	}
+
+}
